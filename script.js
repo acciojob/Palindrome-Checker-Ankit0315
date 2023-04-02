b1d7db2e3743dffc86ff0d431525436e73252945
ANKIT KUMAR RAI <117643219+Ankit0315@users.noreply.github.com>
2023-04-02 11:06:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 // complete the given function
 
 function palindrome(str){
-	for(let i=0; i<str.length/2; i++)
-		{
-			if(str[i!=str[str.length-1-i]])
-			{
-				return false;
-			}
-		}
-	return true;
+	
+ str.toLowerCase();
+ const string = str.toLowerCase();
+ const len = str.length;
 
+    // loop through half of the string
+    for (let i = 0; i < len / 2; i++) {
+
+        if (string[i] !== string[len - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
 }
 module.exports = palindrome
